13dacf5a061d870bdbec4768dda13f06bd339756
thayron matos <thayronrt@gmail.com>
2020-02-05 13:19:10 -0300

    foram criados as ocorrências e vinculados com os botões raios

--- javajs.js
+++ javajs.js
@@ -31,13 +31,13 @@ function Enviar() {
         if (select == 1) {
             texoArea.value = `CLIENTE ${nome} COM DUVIDA NA EMISSÃO DO CERTIFICADO A1, FEITO ACESSO REMOTO
 
-            REALIZADO A BAIXA
-            INSTALADO CERTIFICADO NA MAQUINA DEIXANDO A OPÇÃO DE EXPORTAÇÃO DA CHAVE.
-            ORIENTADO SOBRE O BACKUP
+REALIZADO A BAIXA
+INSTALADO CERTIFICADO NA MAQUINA DEIXANDO A OPÇÃO DE EXPORTAÇÃO DA CHAVE.
+ORIENTADO SOBRE O BACKUP
             
-            TESTADO NO PORTAL E-CAC TUDO OK
+TESTADO NO PORTAL E-CAC TUDO OK
             
-            SOLICITAÇÃO: ${solicitacao}
+SOLICITAÇÃO: ${solicitacao}
             `
             copiar();
             res.innerHTML = "NÃO SE ESQUEÃO DE FALAR SOBRE A INPORTANCIA DO BACKUP"
@@ -48,26 +48,19 @@ function Enviar() {
         } else if (select == 3) {
             texoArea.value = `CLIENTE ${nome} COM DUVIDA NA ALTERAÇÃO DA SENHA DE SUA MÍDIA, FEITO ACESSO REMOTO
 
-            REALIZADO O PROCEDIMENTO DE ALTERAÇÃO DA SENHA
+REALIZADO O PROCEDIMENTO DE ALTERAÇÃO DA SENHA
             
-            FEITO O TESTE NO PORTAL E-CAC TUDO OK
+FEITO O TESTE NO PORTAL E-CAC TUDO OK
             
-            SOLICITAÇÃO: ${solicitacao}
+SOLICITAÇÃO: ${solicitacao}
             `
             copiar();
             res.value = "NUNCA FORNEÇA A SENHA DO ADMINISTRADOR PARA O CLIENTE"
         } else if (select == 4) {
-            texoArea.value = `CLIENTE ${nome} INFORMA QUE ESTA COM A MÍDIA BLOQUEADA, QUESTIONADO SE TINHA SENHA O ADMINISTRADOR (CAIXA DE CD), CLIENTE AFIRMA QUE SIM, FEITO ACESSO REMOTO.
-
-            REALIZADO O PROCEDIMENTO DE DESBLOQUEIO COM A SENHA INFORMADO PELO O CLIENTE
-            
-            FEITO O TESTE NO PORTAL E-ECAC TUDO NORMAL
-            
-            SOLICITAÇÃO: ${solicitacao}
-            `
+            texoArea.value = ``
             copiar();
 
-            res.innerHTML = `NUNCA FORNEÇA A SENHA DO ADMINISTRADOR PARA O CLIENTE`;
+            res.innerHTML = erro;
 
         } else if (select == 5) {
 
@@ -79,40 +72,160 @@ function Enviar() {
         if (select == 1) {
             texoArea.value = `CLIENTE ${nome} COM DUVIDA NA EMISSÃO DO CERTIFICADO A3, FEITO ACESSO REMOTO
 
-            INSTALA O DRIVER
-            REALIZADO A BAIXA DO CERTIFICADO
+INSTALA O DRIVER
+REALIZADO A BAIXA DO CERTIFICADO
             .
-            TESTADO NO PORTAL E-CAC TUDO OK
+TESTADO NO PORTAL E-CAC TUDO OK
             
-            SOLICITAÇÃO: ${solicitacao}`;
+SOLICITAÇÃO: ${solicitacao}`;
             copiar();
             res.innerHTML = "SEMPRE REALISE O TENTE DO CERTIFICADO"
         } else if (select == 2) {
 
             texoArea.value = `CLIENTE ${nome} COM DUVIDA NA INSTALAÇÃO DO DRIVER, FEITO ACESSO REMOTO
 
-            INSTADO DRIVER
+INSTADO DRIVER
             
-            CONFIGURADO DLL NO MOZILLA
+CONFIGURADO DLL NO MOZILLA
             
-            TESTADO NO PORTAL E-CAC TUDO OK
+TESTADO NO PORTAL E-CAC TUDO OK
             
-            SOLICITAÇÃO: ${solicitacao}
+SOLICITAÇÃO: ${solicitacao}
             `
+        } else if (select == 3) {
+            texoArea.value = `CLIENTE ${nome} ENTROU EM CONTATO COM DUVIDA NA ALTERAÇÃO DA SENHA DE SUA MÍDIA, FEITO ACESSO REMOTO.
+            
+FOI REALIZADO O PROCEDIMENTO DE ALTERAÇÃO DA SENHA.
+
+FOI FEITO O TESTE NO PORTAL E-CAC E TUDO OK.
+
+SOLICITAÇÃO: ${solicitacao}
+            `
+        } else if (select == 4) {
+            texoArea.value = `CLIENTE ${nome} INFORMA QUE ESTAVA COM SUA MÍDIA BLOQUEADA, QUESTIONADO SE TINHA A SENHA DO ADMINISTRADOR (CAIXA DE CD), CLIENTE ${nome} AFIRMA QUE SIM, FEITO  O ACESSO REMOTO.
+            
+REALIZADO O PROCEDIMENTO DE DESBLOQUEIO COM A SENHA INFORMADO PELO CLIENTE.
+
+FOI FEITO O TESTE NO PORTAL DO E-CAC E TUDO OK.
+
+SOLICITAÇÃO: ${solicitacao}
+            `
+            copiar();
+
+            res.innerHTML = "NUNCA FORNEÇA A SENHA DO ADMINISTRADOR PARA O CLIENTE"
+
+        } else if (select == 5) {
+            texoArea.value = "";
+            copiar();
+            res.innerHTML = erro;
+
         }
 
 
     } else if (sel[2].checked) {
-
         if (select == 1) {
-            texoArea.value = `CLIENTE ${nome} ENTROU EM CONTATO PEDINDO AUXILIO PARA A INSTALAÇÃO DOS DRIVERS`
+            texoArea.value = `CLIENTE ${nome} COM DUVIDA NA EMISSÃO DO CERTIFICADO A3, FEITO ACESSO REMOTO
+
+INSTALA O DRIVER
+REALIZADO A BAIXA DO CERTIFICADO
+            .
+TESTADO NO PORTAL E-CAC TUDO OK
+            
+SOLICITAÇÃO: ${solicitacao}`;
+            copiar();
+            res.innerHTML = "SEMPRE REALISE O TENTE DO CERTIFICADO"
+        } else if (select == 2) {
+
+            texoArea.value = `CLIENTE ${nome} COM DUVIDA NA INSTALAÇÃO DO DRIVER, FEITO ACESSO REMOTO
+
+INSTADO DRIVER
+            
+CONFIGURADO DLL NO MOZILLA
+            
+TESTADO NO PORTAL E-CAC TUDO OK
+            
+SOLICITAÇÃO: ${solicitacao}
+            `
+        } else if (select == 3) {
+            texoArea.value = `CLIENTE ${nome} ENTROU EM CONTATO COM DUVIDA NA ALTERAÇÃO DA SENHA DE SUA MÍDIA, FEITO ACESSO REMOTO.
+            
+FOI REALIZADO O PROCEDIMENTO DE ALTERAÇÃO DA SENHA.
+
+FOI FEITO O TESTE NO PORTAL E-CAC E TUDO OK.
+
+SOLICITAÇÃO: ${solicitacao}
+            `
+        } else if (select == 4) {
+            texoArea.value = `CLIENTE ${nome} INFORMA QUE ESTAVA COM SUA MÍDIA BLOQUEADA, QUESTIONADO SE TINHA A SENHA DO ADMINISTRADOR (CAIXA DE CD), CLIENTE ${nome} AFIRMA QUE SIM, FEITO  O ACESSO REMOTO.
+            
+REALIZADO O PROCEDIMENTO DE DESBLOQUEIO COM A SENHA INFORMADO PELO CLIENTE.
+
+FOI FEITO O TESTE NO PORTAL DO E-CAC E TUDO OK.
+
+SOLICITAÇÃO: ${solicitacao}
+            `
+            copiar();
+
+            res.innerHTML = "NUNCA FORNEÇA A SENHA DO ADMINISTRADOR PARA O CLIENTE"
+
+        } else if (select == 5) {
+            texoArea.value = "";
+            copiar();
+            res.innerHTML = erro;
+
         }
-        console.log('entrou no cartão')
-        res.innerHTML = "entrou no cartão"
+
 
     } else if (sel[3].checked) {
-        console.log('entrou no birdid')
-        res.innerHTML = "ENTROU NO BIRDID"
+
+        if (select == 1) {
+            texoArea.value = `CLIENTE ${nome} ENTROU EM CONTATO PEDINDO AUXILIO PARA EMITIR O CERTIFICADO, CLIENTE FOI ORIENTADO NO PROCESSO.
+            
+ CLIENTE FOI ORIENTADO SOBRE A IMPORTANCIA DA SENHA DO PORTAL.
+
+ FOI SINCRONIZADO O CELULAR, 
+
+ FOI EMITIDO SEM ERROS, TESTADO NO PORTAL E-CAC E TUOD OK.
+
+ SOLICITAÇÃO: ${solicitacao}
+
+                `
+            copiar();
+
+            res.innerHTML = "SEMPRE INFORME O CLIENTE SOBRE A IMPORTANCIA DA SENHA DO PORTAL"
+        } else if (select == 2) {
+            texoArea.value = `CLINETE ${nome} ENTROU EM CONTATO PEDINDO AUXILIO PARA INSTALAR O BIRDID DESKTOP.
+FOI INSTALADO O DRIVE, SINCRONIZADO O USUARIO.
+
+FOI TESTADO O CERTIFICADO NO PORTAL E-CAC E TUDO OK.
+
+SOLICITAÇÃO: ${solicitacao}
+`
+            copiar();
+            res.innerHTML = "SEMPRE FAÇA O TESTE DO CERTIFICADO"
+        } else if (select == 3) {
+            texoArea.value = `CLIENTE ${nome} ENTROU EM CONTATO ALEGANDO QUE NÃO SABIA A SENHA DO PORTAL E QUERIA TROCAR A SENHA.
+            
+CLIENTE ORIENTADO QUE ESSA SENHA ERA INPOSSIVEL DE RECUPERAR POIS ERA CRIADO POR QUEM EMITIU O CERTIFICADO.
+
+CLIENTE ORIENADO QUE NESSE CASO E SOMENTE ADQUIRINDO OUTRO CERIFICADO.
+
+CLIENTE ORIENTADO QUE ASSIM QUE TIVER UMA NOVA SOLICITAÇÃO O MESMO TERIA QUE ENTRAR EM CONTATO COM O SUPORTE PARA QUE FOSSE APAGADO A CONTA ANTERIOR PARA EVITAR QUE FOSSE EMITIDO O CERTIFICADO NA CONTATO QUE O MESMO NÃO POSSIA A SENHA DE ACESSO AO PORTAL
+
+SOLICITAÇÃO: ${solicitacao}
+`
+
+            copiar();
+            res.innerHTML = "SEMPRE INFORME O CLIENTE SOBRE A IMPORTANCIA DA SENHA DO PORTAL"
+        } else if (select == 4) {
+            texoArea.value = ""
+            copiar();
+            res.innerHTML = erro;
+        } else if (select == 5) {
+            texoArea.value = "";
+            copiar();
+            res.innerHTML = erro;
+        }
 
     }
 
